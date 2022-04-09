07a8ab055e462f7a1b7f7115667e14d2f7351c88
Adil Haneef <99606693+A0D1I2L3@users.noreply.github.com>
2022-04-09 23:25:33 +0530

    1st commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from venv.variables import *
+import discord
+
+client=discord.Client()
+
+
+@client.event
+async def on_ready():
+    general_channel = client.get_channel(947433833660317709)
+
+    await general_channel.send('Bot online')  
+
+
+@client.event
+async def on_message(message):
+
+    if message.content.startswith ("!version"):
+        general_channel = client.get_channel(947433833660317709)
+
+        MyEmbed = discord.Embed(title="Current version" , description="The version is beta" , colour= 0x00FF00 )
+
+        MyEmbed.add_field(name="project Name",value="project pseudobot",inline=False)
+        MyEmbed.add_field(name="Date released",value="Tomorrow",inline=False)
+        MyEmbed.set_footer(text="Heheheeheheheh")
+        MyEmbed.set_author(name="@A0D1I2L3#1746")
+
+
+
+
+        await general_channel.send(embed=MyEmbed) 
+
+client.run(TOKEN)
+
