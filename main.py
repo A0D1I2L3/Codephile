f3b102fcbbd2bf4f0a6b7762efbb73194eb15aa1
Adil Haneef <99606693+A0D1I2L3@users.noreply.github.com>
2022-04-14 21:22:25 +0530

    Test commit

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def version(context):
     MyEmbed = discord.Embed(title="Current version" , description="The version is beta" , colour= 0x00FF00 )
 
     MyEmbed.add_field(name="project Name",value="project pseudobot",inline=False)
-    MyEmbed.add_field(name="Date released",value="Tomorrow",inline=False)
+    MyEmbed.add_field(name="Date released",value="Tomorrow",inline=True)
     MyEmbed.set_footer(text="Heheheeheheheh")
 
     await context.message.channel.send(embed=MyEmbed)
